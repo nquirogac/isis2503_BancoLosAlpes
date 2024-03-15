e7360765412af9302f1f0e75aaa3fc18ad407144
Trodri <t.rodriguez@uniandes.edu.co>
2024-03-15 13:25:30 -0500

    migrations cliente & add AppClientes

--- isis2503_BancoLosAlpes/settings.py
+++ isis2503_BancoLosAlpes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'solicitudes',
     'usuarios',
+    'clientes',
+    'administradores'
 ]
 
 MIDDLEWARE = [
--- solicitudes/models.py
+++ solicitudes/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from usuarios.models import Usuario
+from clientes.models import Cliente
 
 class Solicitud(models.Model):
-    user = models.ForeignKey(Usuario, on_delete = models.CASCADE, default = None)
+    user = models.ForeignKey(Cliente, on_delete = models.CASCADE, default = None)
     creationDate = models.DateTimeField(auto_now_add = True)
     closeDate = models.DateField(null = True, blank= True, default = None)
     status = models.CharField(max_length = 50)
